3abf94609588cfce99a37884e5c67acb2a6cc30e
luccas-arruda <luccasarruda@hotmail.com.br>
2025-02-14 21:32:29 -0300

    feat(produto): adicionar classe de produto

--- Domain/Entities/Produto.cs
+++ Domain/Entities/Produto.cs
@@ -2,5 +2,40 @@
 
 public class Produto
 {
+    private int _id;
+    private string _nome;
+    private string _descricao;
+    private double _valor;
     
+    public int Id
+    {
+        get { return _id; }
+        set { _id = value; }
+    }
+    
+    public string Nome
+    {
+        get { return _nome; }
+        set { _nome = value; }
+    }
+
+    public string Descricao
+    {
+        get { return _descricao; }
+        set { _descricao = value; }
+    }
+
+    public double Valor
+    {
+        get { return _valor; }
+        set { _valor = value; }
+    }
+
+    public Produto(int id, string nome, string descricao, double valor)
+    {
+        this.Id = id;
+        this.Nome = nome;
+        this.Descricao = descricao;
+        this.Valor = valor;
+    }
 }
