b38530ac9138bba6fb18f2da706d930e22f25ddd
Gabriel Brandino <gabriel.brandino01@gmail.com>
2025-02-14 21:32:38 -0300

    feat(fornecedor): adcionar classe fornecedor

---
+++ Domain/Entities/Fornecedor.cs
@@ -0,0 +1,24 @@
+﻿namespace CRUD.Domain.Entities;
+
+public class Fornecedor
+{
+    public string Nome {get; set;}
+
+    public string Cnpj {get; set;}
+    
+    public string Telefone {get; set;}
+    
+    public string Email {get; set;}
+
+    public string Cep {get; set;}
+
+    public Fornecedor(string nome, string cnpj, string telefone, string email, string cep){
+        this.Nome = nome;
+        this.Cnpj = cnpj;
+        this.Telefone = telefone;
+        this.Email = email;
+        this.Cep = cep;
+    }
+    
+    public void CadastrarFornecedor(){}
+}
